7c56da70c3607db931de6e09bfe119cabfc35ba1
friskaawr <zahfriska@gmail.com>
2019-07-12 13:17:23 +0700

    add handler

--- client/js/jobs.js
+++ client/js/jobs.js
@@ -5,6 +5,8 @@ function allJobs() {
     $('.searching').click(function () {
         let keyword = $('#keyword-search').val()
         $('#form-input-keyword').empty()
+        $('#listJob').empty()
+        $('#form-input-keyword-get-list').hide()
         $('#form-input-keyword').append(
             `<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>`
         )
@@ -15,29 +17,38 @@ function allJobs() {
         })
             .done(function (data) {
                 console.log(data);
-                foundJobs = data
-                $('#form-input-keyword').empty()
-                $('#listJob').empty()
-                $('#form-input-keyword-get-list').show()
-                data.forEach(element => {
-                    let description = element.description.split('\n')
-                    $('#listJob').append(`
-                    <div id="form-list" class="card" style="margin-top:5%">
-                        <div class="card-header">
-                        ${element.title}
+                
+                if(data.length > 0){
+                    foundJobs = data
+                    $('#form-input-keyword').empty()
+                    $('#listJob').empty()
+                    $('#form-input-keyword-get-list').show()
+                    data.forEach(element => {
+                        let description = element.description.split('\n')
+                        $('#listJob').append(`
+                        <div id="form-list" class="card" style="margin-top:5%">
+                            <div class="card-header">
+                            ${element.title}
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title"> ${element.company} </h5>
+                                <small><a href=${element.company_url}> View Website </a></small>
+                                <p>location : ${element.location} </p>
+                                ${description[0]}
+                                <a href="#" onclick="detailJobs('${element.id}')" class="btn btn-info">See Detail</a>
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <h5 class="card-title"> ${element.company} </h5>
-                            <small><a href=${element.company_url}> View Website </a></small>
-                            <p>location : ${element.location} </p>
-                            ${description[0]}
-                            <h6>How To Apply</h6>
-                            ${element.how_to_apply}
-                            <a href="#" onclick="detailJobs('${element.id}')" class="btn btn-primary">See Detail</a>
-                        </div>
-                    </div>
-                    `)
-                });
+                        `)
+                    });
+                } else {
+                    console.log("gak ada data di keywordnya");
+                    $('#form-input-keyword').empty()
+                    $('#listJob').empty()
+                    $('#form-input-keyword-get-list').show()
+                    $('#listJob').append(
+                        `<h1> not found </h1>`
+                    )
+                }
             })
             .fail(function (err) {
                 console.log(err);
@@ -91,10 +102,12 @@ function detailJobs(id){
         <div class="col-12" id="googleMap" style="width:80vw;height:50vh;"></div> 
         <h1 class="col-8" style="margin-top:5%;">${detail.title}</h1>
         <div class="col-12" style="text-alignment: justify;"> ${detail.description} </div>
-        <button id="searching" type="button" class="btn btn-info" class="col-12" style="width: 100%; margin-bottom: 5%; margin-top: 3%;"> Apply </button>
+        <h6 class="col-12">How To Apply</h6> 
+        <p class="col-12"> ${detail.how_to_apply} </p> 
     `)
 }
 
+//  <button id="searching" type="button" class="btn btn-info" class="col-12" style="width: 100%; margin-bottom: 5%; margin-top: 3%; disabled"> Apply </button>  
 function homePage(){
     $('.detail-jobs').empty()
     $('#form-input-keyword').show()
